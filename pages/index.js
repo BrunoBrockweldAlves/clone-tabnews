5caed1b4c0025d39c0e5a444177a3dc08d898bd2
Bruno Brockweld Alves <55096120+BrunoBrockweldAlves@users.noreply.github.com>
2024-08-11 13:43:07 +0000

    Add tibia video

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
   return <h1>Marina, vc é o meu amor 💞</h1>;
+
+  <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/YOa0F0LWGPg?si=TTGXiUO-5Env1KlK"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerpolicy="strict-origin-when-cross-origin"
+    allowfullscreen
+  ></iframe>;
 }
 
 export default Home;
