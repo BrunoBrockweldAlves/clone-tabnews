aa8337ba14b0c13906ff1ae0c82d5d878ac4dedd
Bruno Brockweld Alves <55096120+BrunoBrockweldAlves@users.noreply.github.com>
2024-08-11 13:46:18 +0000

    Fix error

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 function Home() {
-  return <h1>Marina, vc é o meu amor 💞</h1>;
-
-  <iframe
-    width="560"
-    height="315"
-    src="https://www.youtube.com/embed/YOa0F0LWGPg?si=TTGXiUO-5Env1KlK"
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    referrerpolicy="strict-origin-when-cross-origin"
-    allowfullscreen
-  ></iframe>;
+  return (
+    <div>
+      <h1>Marina, vc é o meu amor 💞</h1>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/YOa0F0LWGPg?si=TTGXiUO-5Env1KlK"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 }
 
 export default Home;
